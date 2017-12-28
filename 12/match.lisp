875acdfd17646940b205101b65ca679c2adef4e2
David Alkire <david.alkire@gmail.com>
2017-12-27 20:13:53 -0500

    12.2

--- 12/match.lisp
+++ 12/match.lisp
@@ -3,3 +3,11 @@
 (defpackage :match
   (:use :common-lisp))
 (in-package :match)
+
+;;; 12.2 In your match file, define a function variablep that takes one
+;;; symbol as an argument and returns T if the first character of the
+;;; symbol’s name is #\? and returns NIL otherwise. If you have a modern
+;;; Lisp development environment, develop and test this function using the
+;;; two-window approach described in this chapter.
+(defun variablep (s)
+  (char= #\? (char (symbol-name s) 0)))
