495aefb71552a17dae28c685605df6f8b82e6f05
David Alkire <david.alkire@gmail.com>
2017-12-27 20:28:43 -0500

    12.6

--- 12/calculator.lisp
+++ 12/calculator.lisp
@@ -3,3 +3,11 @@
 (defpackage :calculator
   (:use :common-lisp))
 (in-package :match)
+
+;;; 12.6 In your calculator file, define the function combine-expr to take
+;;; an arithmetic operator, an operand, and a list representing an arithmetic
+;;; expression and return the expression with the operator and operand
+;;; applied to the first member of the expression. For example,
+;;; (combine-expr ’+ 3 ’(5 - 6 * 8)) should evaluate to ((3 + 5) - 6 * 8).
+(defun combine-expr (operator operand expression)
+  (cons (list operand operator (first expression)) (rest expression)))
