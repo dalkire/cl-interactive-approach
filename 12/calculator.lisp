16ee5e0b90e1427f05f739bf9fcf59ac8f31eb04
David Alkire <david.alkire@gmail.com>
2017-12-27 20:21:12 -0500

    12.5

---
+++ 12/calculator.lisp
@@ -0,0 +1,5 @@
+;;; 12.5 Begin a file named calculator. Set it up so that the functions
+;;; in it will be defined in the calculator package.
+(defpackage :calculator
+  (:use :common-lisp))
+(in-package :match)
