bfeb69894b434f81b4711166b8d09d4c36998376
David Alkire <david.alkire@gmail.com>
2017-12-28 17:09:08 -0500

    14.8

--- match.lisp
+++ match.lisp
@@ -16,10 +16,19 @@
 ;;; 13.13 In your file match, which you began for Exercise 12.1, define the
 ;;; function (match-element e1 e2) to return T if its two arguments are eql
 ;;; or if either of them is a variable as recognized by match::variablep.
+
+;;; 14.8 Also in your match file, redefine (match-element e1 e2) so that it
+;;; returns True if the two arguments are eql or either argument is ?; if
+;;; either argument is a variable (as recognized by variablep), match-element
+;;; should return a two-member list whose first member is the variable and
+;;; whose second member is the other argument; otherwise, match-element
+;;; should return NIL.
 (defun match-element (e1 e2)
   "Returns T if its two arguments are eql or if either of them is a variable as recognized by variablep."
-  (or (eql e1 e2)
-      (or (variablep e1) (variablep e2))))
+  (cond ((eql e1 e2) t)
+        ((or (eql e1 '?) (eql e2 '?)) t)
+        ((variablep e1) (list e1 e2))
+        ((variablep e2) (list e2 e1))))
 
 ;;; 14.7 In your match file, define the function dont-care to return True
 ;;; if its argument is a question-mark symbol and NIL in any other case.
