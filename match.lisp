571769d9ce87b1e8b9fcd2118759f2954c0b1501
David Alkire <david.alkire@gmail.com>
2017-12-28 16:48:02 -0500

    13.13

--- match.lisp
+++ match.lisp
@@ -10,4 +10,13 @@
 ;;; Lisp development environment, develop and test this function using the
 ;;; two-window approach described in this chapter.
 (defun variablep (s)
-  (char= #\? (char (symbol-name s) 0)))
+  "Takes a symbol S as an argument and returns T if the first character of the symbol’s name is #\? and returns NIL otherwise."
+  (and (symbolp s) (char= #\? (char (symbol-name s) 0))))
+
+;;; 13.13 In your file match, which you began for Exercise 12.1, define the
+;;; function (match-element e1 e2) to return T if its two arguments are eql
+;;; or if either of them is a variable as recognized by match::variablep.
+(defun match-element (e1 e2)
+  "Returns T if its two arguments are eql or if either of them is a variable as recognized by variablep."
+  (or (eql e1 e2)
+      (or (variablep e1) (variablep e2))))
